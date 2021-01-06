28ebaacc360421c861fea153a9cacc7e557def92
Rocco <roccomaniscalco2001@gmail.com>
2021-01-06 11:09:41 -0500

    modified mongoose connection to remove deprecation warning

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -14,15 +14,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
-app.get("/exercise", (req,res) => {
-  res.sendFile(path.join(__dirname, "/public/exercise.html"))
-})
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
 
-app.get("/stats", (req,res) => {
-  res.sendFile(path.join(__dirname, "/public/stats.html"))
-})
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/stats.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:3000`);
