0d442c081d1285469126e5e4266bd59c1e71515d
Rocco <roccomaniscalco2001@gmail.com>
2021-01-07 22:28:13 -0500

    created get recent workouts API route

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.listen(PORT, () => {
 // API ROUTES
 app.get("/api/workouts", async (req, res) => {
   try {
-    const latestWorkout = await db.Workout.find();
-    res.status(200).json(latestWorkout);
+    const workouts = await db.Workout.find();
+    res.status(200).json(workouts);
   } catch (err) {
-    res.status(500).send("Internal Server Error");
+    res.status(500).json("Internal Server Error");
   }
 });
 
@@ -49,7 +49,7 @@ app.put("/api/workouts/:id", async (req, res) => {
     });
     res.status(200).json(updatedWorkout);
   } catch (err) {
-    res.status(500).send("Internal Server Error");
+    res.status(500).json("Internal Server Error");
   }
 });
 
@@ -58,6 +58,15 @@ app.post("/api/workouts", async (req, res) => {
     const newWorkout = await db.Workout.create(req.body);
     res.status(200).json(newWorkout);
   } catch (err) {
-    res.status(500).send(err);
+    res.status(500).json(err);
+  }
+});
+
+app.get("/api/workouts/range", async (req, res) => {
+  try {
+    const latestWorkouts = await db.Workout.find().limit(7).sort({day:-1})
+    res.json(latestWorkouts);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
